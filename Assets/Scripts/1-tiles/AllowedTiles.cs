336787e69bc757814be899659a319e8b464992eb
Aviv Shevach <shevach.aviv@gmail.com>
2024-02-14 10:16:13 +0200

    up1

---
+++ Assets/Scripts/1-tiles/AllowedTiles.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+/**
+ * This component just keeps a list of allowed tiles.
+ * Such a list is used both for pathfinding and for movement.
+ */
+public class AllowedTiles : MonoBehaviour  {
+    [SerializeField] TileBase[] allowedTiles = null;
+   // [SerializeField] TileBase[] allTiles = null;
+
+
+    public bool Contains(TileBase tile) {
+        GameObject boat = GameObject.Find("boat");
+        GameObject goat = GameObject.Find("goat");
+        GameObject pick = GameObject.Find("pick");
+        if ( boat == null ) {
+            if (tile.ToString().Contains("sea"))
+                return true;
+        }
+        if ( goat == null || pick == null) {
+            if (tile.ToString().Contains("mountains"))
+                return true;
+        }
+
+        return allowedTiles.Contains(tile);
+    }
+
+    public void Set(TileBase[] new_tiles) {
+        allowedTiles = new_tiles;
+    }
+
+
+  //  public TileBase[] GetAll() { return allTiles;  }
+    public TileBase[] Get() { return allowedTiles;  }
+
+}
